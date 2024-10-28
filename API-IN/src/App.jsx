ac46e9eab5d584d8a4dfe3614050612fd9ad059d
nishanthspk <spknishanth2004@gmail.com>
2024-10-28 22:20:04 +0530

    div

--- API-IN/src/App.jsx
+++ API-IN/src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-       
-          
-       
-      
-         
-        
-      </div>
+     
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
