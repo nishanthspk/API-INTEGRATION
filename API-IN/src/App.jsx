a18b8844abc5111a08023d22a7920efce0321c1d
nishanthspk <spknishanth2004@gmail.com>
2024-10-28 22:18:49 +0530

    anchor

--- API-IN/src/App.jsx
+++ API-IN/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           
-        </a>
+       
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
