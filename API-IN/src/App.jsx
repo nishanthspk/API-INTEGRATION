eab2ebb7a80330a6a8efa7aeaf7326ba9b8791f9
nishanthspk <spknishanth2004@gmail.com>
2024-10-29 23:25:01 +0530

    text

--- API-IN/src/App.jsx
+++ API-IN/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className=''>BOOK API</h1>
+        <h1 className='text'>BOOK API</h1>
         <Button onClick={App}></Button>
       </div> 
     </>
