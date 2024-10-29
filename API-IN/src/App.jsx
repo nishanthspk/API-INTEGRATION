3204ed4944cb2862ff943bade2b367f7135ddac8
nishanthspk <spknishanth2004@gmail.com>
2024-10-29 23:18:38 +0530

    class

--- API-IN/src/App.jsx
+++ API-IN/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-black'>BOOK API</h1>
+        <h1 >BOOK API</h1>
         <Button onClick={App}></Button>
       </div> 
     </>
