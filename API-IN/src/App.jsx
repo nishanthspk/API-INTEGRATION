a5e01a8f8ea2ff121f90de7d6aae5e09f1b4ffe5
nishanthspk <spknishanth2004@gmail.com>
2024-10-29 22:50:46 +0530

    comment

--- API-IN/src/App.jsx
+++ API-IN/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 const App =() => {
   async function apicall() {
     const response = await fetch(
-       'https://potterapi-fedeperin.vercel.app/es/characters?search=Weasley'
+      //  'https://potterapi-fedeperin.vercel.app/es/characters?search=Weasley'
     );
 
   const data = await response.json();
