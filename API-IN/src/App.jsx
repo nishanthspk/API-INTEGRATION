aedb78e84a3a33651328325e354d5bcd32cd116e
nishanthspk <spknishanth2004@gmail.com>
2024-10-29 23:10:24 +0530

    usestate

--- API-IN/src/App.jsx
+++ API-IN/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-const App =() => {
+const DApp =() => {
   async function apicall() {
     const response = await fetch(
       //  'https://potterapi-fedeperin.vercel.app/es/characters?search=Weasley'
