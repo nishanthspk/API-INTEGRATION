8d8bc6c82eda753788752827001b0d9fb636a97c
nishanthspk <spknishanth2004@gmail.com>
2024-10-29 23:11:41 +0530

    space

--- API-IN/src/App.jsx
+++ API-IN/src/App.jsx
@@ -1,18 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
-const DApp =() => {
-  async function apicall() {
-    const response = await fetch(
-      //  'https://potterapi-fedeperin.vercel.app/es/characters?search=Weasley'
-    );
 
-  const data = await response.json();
-  console.log(data);
-}
-}
 
 function App() {
+
   const [count, setCount] = useState(0)
   return (
     <>
